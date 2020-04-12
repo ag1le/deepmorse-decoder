cc924a2b208e7accaa4d0ef5391efd237e82fd86
Mauri Niininen <mauri@innomore.com>
2020-04-12 18:39:56 -0400

    Made PEP8 compliant using black

--- specgram.py
+++ specgram.py
@@ -15,14 +15,14 @@ import cv2
 
 ############### Import Modules ###############
 import mic_read
-from morse.MorseDecoder import  Config, Model, Batch, DecoderType
+from morse.MorseDecoder import Config, Model, Batch, DecoderType
 
 
 ############### Constants ###############
-SAMPLES_PER_FRAME = 4 #Number of mic reads concatenated within a single window
-nfft = 256 # NFFT value for spectrogram
-overlap = nfft-56 # overlap value for spectrogram
-rate = mic_read.RATE #sampling rate
+SAMPLES_PER_FRAME = 4  # Number of mic reads concatenated within a single window
+nfft = 256  # NFFT value for spectrogram
+overlap = nfft - 56  # overlap value for spectrogram
+rate = mic_read.RATE  # sampling rate
 
 
 ############### Call Morse decoder ###############
@@ -34,11 +34,17 @@ def infer_image(model, img):
     else:
         print(f"ERROR: img shape:{img.shape}")
 
-# Load the Tensorlow model 
-config = Config('model.yaml')
-model = Model(open("morseCharList.txt").read(), config, decoderType = DecoderType.BestPath, mustRestore=True)
 
-stream,pa = mic_read.open_mic()
+# Load the Tensorlow model
+config = Config("model.yaml")
+model = Model(
+    open("morseCharList.txt").read(),
+    config,
+    decoderType=DecoderType.BestPath,
+    mustRestore=True,
+)
+
+stream, pa = mic_read.open_mic()
 
 
 ############### Functions ###############
@@ -48,9 +54,13 @@ gets the audio data from the microphone
 inputs: audio stream and PyAudio object
 outputs: int16 array
 """
-def get_sample(stream,pa):
-    data = mic_read.get_data(stream,pa)
+
+
+def get_sample(stream, pa):
+    data = mic_read.get_data(stream, pa)
     return data
+
+
 """
 get_specgram:
 takes the FFT to create a spectrogram of the given audio signal
@@ -58,11 +68,19 @@ input: audio signal, sampling rate
 output: 2D Spectrogram Array, Frequency Array, Bin Array
 see matplotlib.mlab.specgram documentation for help
 """
-def get_specgram(signal,rate):
-    arr2D,freqs,bins = specgram(signal,window=np.blackman(nfft),  
-                                Fs=rate, NFFT=nfft, noverlap=overlap,
-                                pad_to=32*nfft   )
-    return arr2D,freqs,bins
+
+
+def get_specgram(signal, rate):
+    arr2D, freqs, bins = specgram(
+        signal,
+        window=np.blackman(nfft),
+        Fs=rate,
+        NFFT=nfft,
+        noverlap=overlap,
+        pad_to=32 * nfft,
+    )
+    return arr2D, freqs, bins
+
 
 """
 update_fig:
@@ -72,68 +90,77 @@ data needs to stay, shifting it left, and appending the new data.
 inputs: iteration number
 outputs: updated image
 """
+
+
 def update_fig(n):
-    data = get_sample(stream,pa)
-    arr2D,freqs,bins = get_specgram(data,rate)
-    
+    data = get_sample(stream, pa)
+    arr2D, freqs, bins = get_specgram(data, rate)
+
     im_data = im.get_array()
     if n < SAMPLES_PER_FRAME:
-        im_data = np.hstack((im_data,arr2D))
+        im_data = np.hstack((im_data, arr2D))
         im.set_array(im_data)
     else:
-        keep_block = arr2D.shape[1]*(SAMPLES_PER_FRAME - 1)
-        im_data = np.delete(im_data,np.s_[:-keep_block],1)
-        im_data = np.hstack((im_data,arr2D))
+        keep_block = arr2D.shape[1] * (SAMPLES_PER_FRAME - 1)
+        im_data = np.delete(im_data, np.s_[:-keep_block], 1)
+        im_data = np.hstack((im_data, arr2D))
         im.set_array(im_data)
 
     # Get the image data array shape (Freq bins, Time Steps)
     shape = im_data.shape
 
     # Find the CW spectrum peak - look across all time steps
-    f = int(np.argmax(im_data[:])/shape[1])
+    f = int(np.argmax(im_data[:]) / shape[1])
 
-    # Create a 32x128 array centered to spectrum peak 
-    if f > 16: 
+    # Create a 32x128 array centered to spectrum peak
+    if f > 16:
         print(f"n:{n} f:{f}")
-        img = cv2.resize(im_data[f-16:f+16][0:128], (128,32)) 
-        if img.shape == (32,128):
-            cv2.imwrite("dummy.png",img)
+        img = cv2.resize(im_data[f - 16 : f + 16][0:128], (128, 32))
+        if img.shape == (32, 128):
+            cv2.imwrite("dummy.png", img)
             img = cv2.transpose(img)
             img, recognized, probability = infer_image(model, img)
             if probability > 0.0000001:
                 print(f"infer_image:{recognized} prob:{probability}")
-    return im,
+    return (im,)
+
 
 def main():
-    
+
     global im
     ############### Initialize Plot ###############
     fig = plt.figure()
     """
     Launch the stream and the original spectrogram
     """
-    stream,pa = mic_read.open_mic()
-    data = get_sample(stream,pa)
-    arr2D,freqs,bins = get_specgram(data,rate)
+    stream, pa = mic_read.open_mic()
+    data = get_sample(stream, pa)
+    arr2D, freqs, bins = get_specgram(data, rate)
     """
     Setup the plot paramters
     """
-    extent = (bins[0],bins[-1]*SAMPLES_PER_FRAME,freqs[-1],freqs[0])
-    
-    im = plt.imshow(arr2D,aspect='auto',extent = extent,interpolation="none",
-                    cmap = 'Greys',norm = None) 
+    extent = (bins[0], bins[-1] * SAMPLES_PER_FRAME, freqs[-1], freqs[0])
 
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
-    plt.title('Real Time Spectogram')
+    im = plt.imshow(
+        arr2D,
+        aspect="auto",
+        extent=extent,
+        interpolation="none",
+        cmap="Greys",
+        norm=None,
+    )
+
+    plt.xlabel("Time (s)")
+    plt.ylabel("Frequency (Hz)")
+    plt.title("Real Time Spectogram")
     plt.gca().invert_yaxis()
-    #plt.colorbar() #enable if you want to display a color bar
+    # plt.colorbar() #enable if you want to display a color bar
 
     ############### Animate ###############
-    anim = animation.FuncAnimation(fig,update_fig,blit = True,
-                                interval=mic_read.CHUNK_SIZE/1000)
+    anim = animation.FuncAnimation(
+        fig, update_fig, blit=True, interval=mic_read.CHUNK_SIZE / 1000
+    )
 
-                                
     try:
         plt.show()
     except:
@@ -144,6 +171,7 @@ def main():
     stream.close()
     pa.terminate()
     print("Program Terminated")
+
 
 if __name__ == "__main__":
     main()
